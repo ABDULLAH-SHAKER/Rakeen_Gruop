485958dadd70e7a5a44be4c998fca8d18e9f635a
ABDULLAH SHAKER <19490374-ABDULLAH-SHAKER@users.noreply.gitlab.com>
2024-12-09 12:34:40 +0300

    create copmonentes and header with link

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 export default function App() {
   return (
-     <BrowserRouter>
-     <Route path='/' element={<Home/>} />
-     <Route path='/sgin-in' element={<SignIn/>} />
-     <Route path='/sgin-Up' element={<SignUp/>} />
-     <Route path='/about' element={<About/>} />
-     <Route path='/profile' element={<Profile/>} />
-     </BrowserRouter>
-  )
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />;
+        <Route path="/sign-in" element={<SignIn />} />;
+        <Route path="/sign-up" element={<SignUp />} />;
+        <Route path="/about" element={<About />} />;
+        <Route path="/profile" element={<Profile />} />;
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
 
 export default function About() {
   return (
